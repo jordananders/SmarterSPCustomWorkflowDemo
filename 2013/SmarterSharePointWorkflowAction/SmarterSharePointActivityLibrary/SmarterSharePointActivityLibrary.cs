95cd63d4da3bc71f77c615ad8e5ceac044a0eb37
Jordan Anderson <janderson@degdigital.com>
2015-10-01 14:24:13 -0500

    Framing up 2013 solutions

---
+++ 2013/SmarterSharePointWorkflowAction/SmarterSharePointActivityLibrary/SmarterSharePointActivityLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+
+namespace SmarterSharePointActivityLibrary
+{
+    class SmarterSharePointActivity : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
